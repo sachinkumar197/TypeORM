e5da55529f219e08e74d9b5fd852d1d67385f62c
Ishant Kumar <trumansagar@gmail.com>
2024-11-01 16:01:43 +0530

    Removed newTS.ts File

--- src/routes/create_client.ts
+++ src/routes/create_client.ts
@@ -1,6 +1,28 @@
-import express, { Request, Response } from "express";
+import express from 'express';
+import { Client } from '../entities/Client';
 
-export const router = express.Router();
+const router = express.Router();
 
-export {router as createClientRouter};
+router.post('/api/client', async (req, res) => {
+	const {
+		firstName,
+		lastName,
+		email,
+		cardNumber,
+		balance,
+	} = req.body;
 
+	const client = Client.create({
+		first_name: firstName,
+		last_name: lastName,
+		email,
+		card_number: cardNumber,
+		balance,
+	});
+
+	await client.save();
+
+	return res.json(client);
+});
+
+export { router as createClientRouter };
